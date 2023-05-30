9abcb7c66f465a24a8f738f598f5169afeeb4bb5
ThiagoLahass <lahassthiagofelippe@gmail.com>
2023-05-30 20:59:07 -0300

    :rocket:Creating project file and makefile

---
+++ acsh.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main( int argc, char* argv[]){
+
+    while (1){
+        printf("acsh> ");
+    }
+
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,15 @@
+PROJ_NAME = acsh
+
+compile:
+	@ gcc acsh.c -o $(PROJ_NAME) 
+
+run: clean compile
+	@ ./$(PROJ_NAME)
+
+val:
+	@ valgrind --leak-check=full ./$(PROJ_NAME)
+
+clean:
+	@ rm -rf *.o $(PROJ_NAME)
+
+
