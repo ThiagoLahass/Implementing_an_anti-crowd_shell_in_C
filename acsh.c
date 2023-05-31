582ef600bc007cc87d9b0b73eb81e2891900770a
ThiagoLahass <lahassthiagofelippe@gmail.com>
2023-05-31 12:20:40 -0300

    :hammer:Rearranging the code

--- acsh.c
+++ acsh.c
@@ -7,6 +7,105 @@
 #include <sys/wait.h>
 #include <ctype.h>
 
+#define MAX_LINE_LENGTH 1000
+#define MAX_COMMAND_LENGTH 200
+#define MAX_COMMANDS 5
+#define MAX_ARGS 3
+
+static void trim(char *str);
+
+void process_command(char* command);
+
+int main( int argc, char* argv[]){
+    char* line_commands = calloc(MAX_LINE_LENGTH, sizeof(char));    // Comando de entrada do usuario no sheel
+    char** commands = calloc(MAX_COMMANDS, sizeof(char*));          // Vetor de comandos ( no maximo MAX_COMMANDS)
+    for(int i = 0; i < MAX_COMMANDS; i++ ){
+        commands[i] = calloc(MAX_COMMAND_LENGTH, sizeof(char));     // Comandos em si
+    }
+    int command_count = 0;                                          // Numero de comandos atual inserido pelo usuario
+    const char delimiter[] = "<3";                                  // Delimitador usado
+
+    while (1){
+
+        // Exibicao do prompt
+        printf("acsh> ");
+
+        /*=============== LEITURA DO COMANDO DE ENTRADA E SEPARACAO DOS COMANDOS ===============*/
+        // Aguarda o comando do usuario e le ele quando inserido 
+        fgets(line_commands, MAX_LINE_LENGTH, stdin);
+        trim(line_commands);
+
+        // Removendo o caractere nova linha ('\n') do final do comando e substitui por \0
+        line_commands[strcspn(line_commands, "\n")] = '\0';
+
+        if (strcmp(line_commands, "exit") == 0) {
+            break;
+        }
+
+        char *token;
+        char *remaining = line_commands;
+        command_count = 0;
+
+        /* função strstr() é usada para localizar a próxima ocorrência do delimitador na string*/
+        while ((token = strstr(remaining, delimiter)) != NULL) {
+            
+            /*Numero de comandos é maior que o permitido*/
+            if( command_count+2 > MAX_COMMANDS ){
+                fprintf(stderr, "ERRO: Numero de comandos é maior que o permitido\n");
+                exit(-1);
+            }
+
+            /*Em seguida, é calculado o comprimento da substring antes do delimitador*/
+            int length = token - remaining;
+
+            /*A função strncpy() é usada para copiar essa substring de tamanho length e salvar comando na string de comandos*/
+            strncpy(commands[command_count], remaining, length);
+            
+            /*Remover espacos em branco no comeco e no final*/
+            trim(commands[command_count]);
+
+            /*Incrementar numero de comandos atual*/
+            command_count++;
+            
+            /*Por fim, a variável remaining é atualizada para apontar para o próximo caractere após o delimitador*/
+            remaining = token + strlen(delimiter);
+        }
+        
+        /*Salvar ultimo comando na string de comandos*/
+        strcpy(commands[command_count], remaining);
+
+        /*Remover espacos em branco no comeco e no final*/
+        trim(commands[command_count]);
+
+        /*Incrementar numero de comandos atual*/
+        command_count++;
+        /*============= FIM (LEITURA DO COMANDO DE ENTRADA E SEPARACAO DOS COMANDOS) =============*/
+
+
+        /*============================= PROCESSAMENTO DOS COMANDOS ===============================*/
+        for(int i = 0; i < command_count; i++){
+            process_command(commands[i]);
+        }
+        /*========================== FIM (PROCESSAMENTO DOS COMANDOS) ============================*/
+
+        break;
+    }
+
+    free(line_commands);
+    for(int i = 0; i < MAX_COMMANDS; i++ ){
+        free(commands[i]);
+    }
+    free(commands);
+
+    return 0;
+}
+
+/*
+Input Test:
+command1 <3 command2 arg1 <3 command3 arg1 arg2 <3 command4
+*/
+
+
 /*========================== TRIM FUNCTION =======================*/
 static void trim(char *str) {
     int start = 0;
@@ -24,97 +123,6 @@ static void trim(char *str) {
 }
 /*======================== END (TRIM FUNCTION) ======================*/
 
-
-#define MAX_COMMANDS_LENGTH 1000
-#define MAX_COMMAND_LENGTH 100
-#define MAX_COMMANDS 5
-#define MAX_ARGS 3
-
-int main( int argc, char* argv[]){
-
-    char* line_commands = calloc(MAX_COMMANDS_LENGTH, sizeof(char));
-    char** commands = calloc(MAX_COMMAND_LENGTH, sizeof(char*));
-    for(int i = 0; i < MAX_COMMANDS; i++ ){
-        commands[i] = calloc(MAX_COMMAND_LENGTH, sizeof(char));
-    }
-    char* args[MAX_ARGS];
-    const char delimiter[] = "<3";
-
-
-    int num_commands = 0; // Numero de comandos atual inserido pelo usuario
-
-    while (1){
-
-        // Exibicao do prompt
-        printf("acsh> ");
-
-        /*=============== LEITURA DO COMANDO DE ENTRADA E SEPARACAO DOS COMANDOS ===============*/
-
-        // Aguarda o comando do usuario e le ele quando inserido 
-        fgets(line_commands, MAX_COMMANDS_LENGTH, stdin);
-
-        // Removendo o caractere nova linha ('\n') do final do comando e substitui por \0
-        line_commands[strcspn(line_commands, "\n")] = '\0';
-
-        char *token;
-        char *remaining = line_commands;
-        num_commands = 0;
-
-        /* função strstr() é usada para localizar a próxima ocorrência do delimitador na string*/
-        while ((token = strstr(remaining, delimiter)) != NULL) {
-            
-            /*Numero de comandos é maior que o permitido*/
-            if( num_commands+2 > MAX_COMMANDS ){
-                fprintf(stderr, "ERRO: Numero de comandos é maior que o permitido\n");
-                exit(-1);
-            }
-
-            /*Em seguida, é calculado o comprimento da substring antes do delimitador*/
-            int length = token - remaining;
-            char substring[length + 1];
-
-            /*A função strncpy() é usada para copiar essa substring em uma variável temporária*/
-            strncpy(substring, remaining, length);
-            substring[length] = '\0';
-            
-            /*Salvar comando na string de comandos*/
-            strcpy(commands[num_commands], substring);
-
-            /*Remover espacos em branco no comeco e no final*/
-            trim(commands[num_commands]);
-
-            /*Incrementar numero de comandos atual*/
-            num_commands++;
-            
-            /*Por fim, a variável remaining é atualizada para apontar para o próximo caractere após o delimitador*/
-            remaining = token + strlen(delimiter);
-        }
-        
-        /*Salvar ultimo comando na string de comandos*/
-        strcpy(commands[num_commands], remaining);
-
-        /*Remover espacos em branco no comeco e no final*/
-        trim(commands[num_commands]);
-
-        /*Incrementar numero de comandos atual*/
-        num_commands++;
-
-        /*============= FIM (LEITURA DO COMANDO DE ENTRADA E SEPARACAO DOS COMANDOS) =============*/
-
-        for(int i = 0; i < num_commands; i++){
-            printf("Command: '%s'\n", commands[i]);
-        }
-    }
-
-    free(line_commands);
-    for(int i = 0; i < MAX_COMMANDS; i++ ){
-        free(commands[i]);
-    }
-    free(commands);
-    return 0;
+void process_command(char* command) {
+    printf("Processando comando '%s' ...\n", command);
 }
-
-/*
-Input Test:
-command1 <3 command2 arg1 <3 command3 arg1 arg2 <3 command4
-*/
